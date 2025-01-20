48991e6c3e1abde0a8115ef4060d784ae090f3a2
Randila Premarathne <randilamenukapremarathne@gmail.com>
2025-01-20 22:35:22 +0530

    Add hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <div>
-      Home Page
+      <Image src={'/hero.jpeg'} alt="hero" width={1300} height={800} className="w-screen h-screen"/>
     </div>
     </>
   );
